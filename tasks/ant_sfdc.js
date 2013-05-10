20e07d0b3fc1c15312ec6424f6fe763249091c04
Kevin O'Hara <kevinohara80@gmail.com>
2013-05-10 16:19:12 -0400

    major overhaul

--- tasks/ant_sfdc.js
+++ tasks/ant_sfdc.js
@@ -8,239 +8,266 @@
 
 'use strict';
 
-var path = require('path');
-var buildfile = path.resolve(__dirname, '../ant/build.xml');
+var path     = require('path');
 var metadata = require('../lib/metadata.json');
+var localTmp = path.resolve(__dirname, '../tmp');
+var localAnt = path.resolve(__dirname, '../ant');
 
 function buildPackageXml(pkg, version) {
   var packageXml = [
     '<?xml version="1.0" encoding="UTF-8"?>',
     '<Package xmlns="http://soap.sforce.com/2006/04/metadata">'
   ];
-
-  Object.keys(pkg).forEach(function(key) {
-    
-    var type = pkg[key];
-    var typeName;
-
-    if(metadata[key.toLowerCase()] && metadata[key.toLowerCase()].xmlType) {
-      typeName = metadata[key.toLowerCase()].xmlType;
-    }
-
-    if(!typeName) { grunt.log.error(key + ' is not a valid metadata type'); }
-    
-    packageXml.push('    <types>');
-    type.forEach(function(t) {
-      packageXml.push('        <members>' + t + '</members>');
+  if(pkg) {
+    Object.keys(pkg).forEach(function(key) {  
+      var type = pkg[key];
+      var typeName;
+      if(metadata[key.toLowerCase()] && metadata[key.toLowerCase()].xmlType) {
+        typeName = metadata[key.toLowerCase()].xmlType;
+      }
+      if(!typeName) { grunt.log.error(key + ' is not a valid metadata type'); }
+      packageXml.push('    <types>');
+      type.forEach(function(t) {
+        packageXml.push('        <members>' + t + '</members>');
+      });
+      packageXml.push('        <name>' + typeName + '</name>');
+      packageXml.push('    </types>');
     });
-    packageXml.push('        <name>' + typeName + '</name>');
-    packageXml.push('    </types>');
-
-  });
-
+  }
   packageXml.push('    <version>' + version + '</version>');
   packageXml.push('</Package>');
-
   return packageXml.join('\n');
 }
 
+// export
+
 module.exports = function(grunt) {
 
-  // ANT DEPLOY
+  function clearLocalTmp() {
+    grunt.file.delete(localTmp, { force: true });
+  }
+
+  function makeLocalTmp() {
+    clearLocalTmp();
+    grunt.file.mkdir(localTmp);
+    grunt.file.mkdir(localTmp + '/ant');
+    grunt.file.mkdir(localTmp + '/src');
+  }
+
+  function parseAuth(options, target) {
+    var un = (!options.useEnv) ? options.user  : process.env.SFUSER;
+    var pw = (!options.useEnv) ? options.pass  : process.env.SFPASS;
+    var tk = (!options.useEnv) ? options.token : process.env.SFTOKEN;
+    if(tk) pw += tk;
+    if(!un) { grunt.log.error('no username specified for ' + target); }
+    if(!pw) { grunt.log.error('no password specified for ' + target); }
+    if(!un || !pw) grunt.fail.warn('username/password error');
+    options.user = un;
+    options.pass = pw;
+    grunt.log.writeln('User -> ' + options.user.green);
+  }
+
+  function runAnt(task, args, done) {
+    grunt.log.debug('ANT CMD: ant ' + args.join(' '));
+    grunt.log.writeln('Starting ' + task + '...');
+    grunt.util.spawn({
+      cmd: 'ant',
+      args: args
+    }, function(error, result, code) {
+      grunt.log.debug(String(result.stdout));
+      if(error) {
+        grunt.log.error(error);
+      } else {
+        grunt.log.ok(task + ' target ' + target + ' successful');
+      }
+      clearLocalTmp();
+      done();
+    });
+  }
+
+  /*************************************
+   * antdeploy task
+   *************************************/
   
   grunt.registerMultiTask('antdeploy', 'Run ANT deploy to Salesforce', function() {
 
+    makeLocalTmp();
+
     var done = this.async();
     var target = this.target.green;
+    var template = grunt.file.read(localAnt + '/antdeploy.build.xml');
 
     var options = this.options({
+      user: false,
+      pass: false,
+      token: false,
       root: 'build',
-      version: '27.0',
-      useEnv: false,         // use env vars for auth (SFUSER, SFPASS, and SFTOKEN)
+      apiVersion: '27.0',
+      serverurl: 'https://login.salesforce.com',
       checkOnly: false,
       runAllTests: false,
-      rollbackOnError: true
+      rollbackOnError: true,
+      useEnv: false
     });
 
     grunt.log.writeln('Deploy Target -> ' + target);
 
-    var un = (!options.useEnv) ? this.data.user  : process.env.SFUSER;
-    var pw = (!options.useEnv) ? this.data.pass  : process.env.SFPASS;
-    var tk = (!options.useEnv) ? this.data.token : process.env.SFTOKEN;
+    parseAuth(options, this.target);
 
-    if(tk) pw += tk;
+    options.tests = this.data.tests || [];
 
-    if(!un) { grunt.log.error('No username specified for ' + this.target); }
-    if(!pw) { grunt.log.error('No password specified for ' + this.target); }
+    var buildFile = grunt.template.process(template, { data: options });
+    grunt.file.write(localTmp + '/ant/build.xml', buildFile);
 
-    if(!un || !pw) grunt.fail.warn('username/password error');
-
-    grunt.log.writeln('User -> ' + un.green);
-
-    var packageXml = buildPackageXml(this.data.pkg, options.version);
+    var packageXml = buildPackageXml(this.data.pkg, options.apiVersion);
     grunt.file.write(options.root + '/package.xml', packageXml);
     
     // build up our cli args
     var args =  [
       '-buildfile',
-      buildfile,
+      localTmp + '/ant/build.xml',
       '-Dbasedir='     + process.cwd(),
-      '-DSFUSER='      + un,
-      '-DSFPASS='      + pw,
-      '-DSERVERURL='   + (this.data.serverurl || 'https://login.salesforce.com'),
-      '-DROOT='        + options.root,
-      '-DCHECKONLY='   + options.checkOnly,
-      '-DRUNALLTESTS=' + options.runAllTests,
-      '-DROLLBACK='    + options.rollbackOnError
-    ];
-
-    if(this.data.tests) {
-      if(typeof this.data.tests !== 'string') {
-        this.data.tests = this.data.test.join(',');
-      }
-      args.push('-DTESTS' + this.data.tests);
-    }
-
-    args.push('deploy');
-
-    grunt.log.debug('ANT CMD: ant ' + args.join(' '));
-    grunt.log.writeln('Starting deploy...');
-
-    grunt.util.spawn({
-      cmd: 'ant',
-      args: args
-    }, function(error, result, code) {
-      grunt.log.debug(String(result.stdout));
-      if(error) {
-        grunt.log.error(error);
-      } else {
-        grunt.log.ok('Deployment target ' + target + ' successful');
-      }
-      done();
-    });
-
-  });
-
-  // ANT DESTROY
-
-  grunt.registerMultiTask('antdestroy', 'Run ANT destructive changes to Salesforce', function() {
-
-    var done = this.async();
-    var target = this.target.green;
-
-    var options = this.options({
-      root: 'build',
-      version: '27.0',
-      useEnv: false
-    });
-
-    grunt.log.writeln('Deploy Target -> ' + target);
-
-    var un = (!options.useEnv) ? this.data.user  : process.env.SFUSER;
-    var pw = (!options.useEnv) ? this.data.pass  : process.env.SFPASS;
-    var tk = (!options.useEnv) ? this.data.token : process.env.SFTOKEN;
-
-    if(tk) pw += tk;
-
-    if(!un) { grunt.log.error('No username specified for ' + this.target); }
-    if(!pw) { grunt.log.error('No password specified for ' + this.target); }
-
-    if(!un || !pw) grunt.fail.warn('username/password error');
-
-    grunt.log.writeln('User -> ' + un.green);
-
-    var packageXml = buildPackageXml(this.data.pkg, options.version);
-    grunt.file.write(options.root + '/destructiveChanges.xml', packageXml);
-    
-    var args =  [
-      '-buildfile',
-      buildfile,
-      '-Dbasedir='     + process.cwd(),
-      '-DSFUSER='      + un,
-      '-DSFPASS='      + pw,
-      '-DSERVERURL='   + (this.data.serverurl || 'https://login.salesforce.com'),
-      '-DROOT='        + options.root,
       'deploy'
     ];
 
-    grunt.log.debug('ANT CMD: ant ' + args.join(' '));
-
-    grunt.log.writeln('Starting deploy...');
-
-    grunt.util.spawn({
-      cmd: 'ant',
-      args: args
-    }, function(error, result, code) {
-      grunt.log.debug(String(result.stdout));
-      if(error) {
-        grunt.log.error(error);
-      } else {
-        grunt.log.ok('Deployment target ' + target + ' successful');
-      }
-      done();
-    });
+    runAnt('deploy', args, done);
 
   });
 
-  // ANT RETRIEVE
+  /*************************************
+   * antdestroy task
+   *************************************/
 
-  grunt.registerMultiTask('antretrieve', 'Run ANT retrieve to get metadata from Salesforce', function() {
+  grunt.registerMultiTask('antdestroy', 'Run ANT destructive changes to Salesforce', function() {
+
+    makeLocalTmp();
 
     var done = this.async();
     var target = this.target.green;
+    var template = grunt.file.read(localAnt + '/antdeploy.build.xml');
 
     var options = this.options({
+      user: false,
+      pass: false,
+      token: false,
+      root: localTmp + '/src',
+      apiVersion: '27.0',
+      useEnv: false
+    });
+
+    grunt.log.writeln('Destroy Target -> ' + target);
+
+    parseAuth(options, this.target);
+
+    options.tests = this.data.tests || [];
+
+    var buildFile = grunt.template.process(template, { data: options });
+    grunt.file.write(localTmp + '/ant/build.xml', buildFile);
+
+    var packageXml = buildPackageXml(null, options.apiVersion);
+    grunt.file.write(localTmp + '/src/package.xml', packageXml);
+
+    var destructiveXml = buildPackageXml(this.data.pkg, options.apiVersion);
+    grunt.file.write(localTmp + '/src/destructiveChanges.xml', destructiveXml);
+
+    var args =  [
+      '-buildfile',
+      localTmp + '/ant/build.xml',
+      '-Dbasedir='     + process.cwd(),
+      'deploy'
+    ];
+
+    runAnt('destroy', args, done);
+
+  });
+
+  /*************************************
+   * antretrieve task
+   *************************************/
+
+  grunt.registerMultiTask('antretrieve', 'Run ANT retrieve to get metadata from Salesforce', function() {
+
+    makeLocalTmp();
+
+    var done = this.async();
+    var target = this.target.green;
+    var template = grunt.file.read(localAnt + '/antretrieve.build.xml');
+
+    var options = this.options({
+      user: false,
+      pass: false,
+      token: false,
       root: 'build',
-      version: '27.0',
+      apiVersion: '27.0',
+      serverurl: 'https://login.salesforce.com',
+      retrieveTarget: false,
+      unpackaged: false,
+      unzip: true,
       useEnv: false
     });
 
     grunt.log.writeln('Retrieve Target -> ' + target);
 
-    var un = (!options.useEnv) ? this.data.user  : process.env.SFUSER;
-    var pw = (!options.useEnv) ? this.data.pass  : process.env.SFPASS;
-    var tk = (!options.useEnv) ? this.data.token : process.env.SFTOKEN;
+    parseAuth(options, this.target);
 
-    if(tk) pw += tk;
+    if(!options.unpackaged) options.unpackaged = path.normalize(options.root + '/package.xml');
+    if(!options.retrieveTarget) options.retrieveTarget = options.root;
 
-    if(!un) { grunt.log.error('no username specified for ' + this.target); }
-    if(!pw) { grunt.log.error('no password specified for ' + this.target); }
+    var buildFile = grunt.template.process(template, { data: options });
+    grunt.file.write(localTmp + '/ant/build.xml', buildFile);
 
-    if(!un || !pw) grunt.fail.warn('username/password error');
-
-    grunt.log.writeln('User -> ' + un.green);
-
-    var packageXml = buildPackageXml(this.data.pkg, options.version);
+    var packageXml = buildPackageXml(this.data.pkg, options.apiVersion);
     grunt.file.write(options.root + '/package.xml', packageXml);
 
     var args =  [
       '-buildfile',
-      buildfile,
+      localTmp + '/ant/build.xml',
       '-Dbasedir='     + process.cwd(),
-      '-DSFUSER='      + un,
-      '-DSFPASS='      + pw,
-      '-DSERVERURL='   + (this.data.serverurl || 'https://login.salesforce.com'),
-      '-DROOT='        + options.root,
-      '-DUNPACKAGED='  + path.normalize(options.root + '/package.xml'),
       'retrieve'
     ];
 
-    grunt.log.debug('ANT CMD: ant ' + args.join(' '));
+    runAnt('retrieve', args, done);
 
-    grunt.log.writeln('Starting retrieve...');
+  });
 
-    grunt.util.spawn({
-      cmd: 'ant',
-      args: args
-    }, function(error, result, code) {
-      grunt.log.debug(String(result.stdout));
-      if(error) {
-        grunt.log.error(error);
-      } else {
-        grunt.log.ok('Retrieve target ' + target + ' successful');
-      }
-      done();
+  /*************************************
+   * antdescribe task
+   *************************************/
+
+  grunt.registerMultiTask('antdescribe', 'Describe all metadata types for an org', function() {
+    
+    makeLocalTmp();
+
+    var done = this.async();
+    var target = this.target.green;
+    var template = grunt.file.read(localAnt + '/antdescribe.build.xml');
+
+    var options = this.options({
+      user: false,
+      pass: false,
+      token: false,
+      apiVersion: '27.0',
+      serverurl: 'https://login.salesforce.com',
+      resultFilePath: '',
+      trace: false,
+      useEnv: false
     });
+
+    grunt.log.writeln('Describe Target -> ' + target);
+
+    parseAuth(options, this.target);
+
+    var buildFile = grunt.template.process(template, { data: options });
+    grunt.file.write(localTmp + '/ant/build.xml', buildFile);
+
+    var args =  [
+      '-buildfile',
+      localTmp + '/ant/build.xml',
+      '-Dbasedir='     + process.cwd(),
+      'describe'
+    ];
+
+    runAnt('describe', args, done);
 
   });
 
